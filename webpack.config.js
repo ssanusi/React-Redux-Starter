82cfd4a2de8ae160d7093a164476df53597d8cc9
Sulaiman Sanusi <sulaimansanusi@Sulaimans-MacBook-Pro.local>
2019-08-24 15:42:32 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(png|jp(e*)g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        exclude: /(src)/,
+        use: [
+          {
+            loader: 'svg-inline-loader',
+            options: {
+              name: '[name]-[hash].[ext]',
+              outputPath: 'svg/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      Components: path.resolve(__dirname, 'src/components'),
+      Utils: path.resolve(__dirname, 'src/utils'),
+      Assets: path.resolve(__dirname, 'src/assets')
+    }
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './public'),
+    compress: true,
+    port: 9001,
+    watchContentBase: true,
+    progress: true
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, './public', 'index.html'),
+      filename: 'index.html'
+    })
+  ]
+};
+
+module.exports = config;
